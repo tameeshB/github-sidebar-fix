8115e5f9af38b7d8153272be1d7c9333d2ad1b95
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2018-05-18 02:41:54 +0530

    init

---
+++ js/github.js
@@ -0,0 +1,8 @@
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+window.onload = function() {
+	var supposedToBeOnTheRight = document.getElementsByClassName("dashboard-sidebar");
+	var supposedToBeOnTheLeft = document.getElementsByClassName("news");
+	insertAfter(supposedToBeOnTheRight[0],supposedToBeOnTheLeft[0]);s
+}
